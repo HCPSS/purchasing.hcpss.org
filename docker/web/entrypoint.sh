8dfefb18875dbd0a826b05c5d69f190a3b85b4af
Brendan Anderson <brendan_anderson@hcpss.org>
2018-04-27 10:15:23 -0400

    add saml

---
+++ docker/web/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+chown -R www-data:www-data /var/www/drupal/files
+chown -R www-data:www-data /var/www/drupal/web/sites/default/files
+chown -R www-data:www-data /var/www/drupal/config
+chown -R www-data:www-data /srv/sqlite
+
+chown root:root /var/www/drupal/web/sites/default/services.yml
+chown root:root /var/www/drupal/web/sites/default/settings.php
+chmod 444 /var/www/drupal/web/sites/default/services.yml
+chmod 444 /var/www/drupal/web/sites/default/settings.php
+
+drush --root=/var/www/drupal/web cr
+drush --root=/var/www/drupal/web updatedb -y
+drush --root=/var/www/drupal/web cim -y
+drush --root=/var/www/drupal/web cr
+
+exec "$@"
