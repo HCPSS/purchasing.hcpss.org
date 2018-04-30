8cfdcd4f1f6bfddbc933a5f96aaa3221a62d0b1a
Brendan Anderson <brendan_anderson@hcpss.org>
2018-04-30 10:00:33 -0400

    Add discount matrix

---
+++ drupal/web/modules/custom/purchasing/src/Model/DiscountMatrix.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\purchasing\Model;
+
+use Drupal\node\NodeInterface;
+
+class DiscountMatrix {
+
+  /**
+   * Discounted line items
+   *
+   * @var NodeInterface[]
+   */
+  private $line_items;
+
+  public function __construct(array $line_items) {
+    foreach ($line_items as $line_item) {
+      if (!$line_item instanceof NodeInterface || $line_item->bundle() != 'discounted_line_item') {
+        throw new \InvalidArgumentException("Line item must be a discounted line item.");
+      }
+    }
+
+    $this->line_items = $line_items;
+  }
+
+  public function getDiscount($title, $vendor) {
+    foreach ($this->line_items as $line_item) {
+      if ($title == $line_item->getTitle()) {
+        if ($vendor->id() == $line_item->field_award->entity->field_vendor->entity->id()) {
+          return $line_item;
+        }
+      }
+    }
+
+    return NULL;
+  }
+
+  /**
+   * Get a list of item names.
+   *
+   * @return array
+   */
+  public function getItemNames() {
+    $names = [];
+    foreach ($this->line_items as $line_item) {
+      $names[$line_item->getTitle()] = $line_item->getTitle();
+    }
+
+    return array_values($names);
+  }
+
+  /**
+   * Get an array of vendors.
+   *
+   * @return NodeInterface[]
+   */
+  public function getVendors() {
+    $vendors = [];
+    foreach ($this->line_items as $line_item) {
+      $vendor = $line_item->field_award->entity->field_vendor->entity;
+      $vendors[$vendor->id()] = $vendor;
+    }
+
+    return array_values($vendors);
+  }
+}
---
+++ drupal/web/modules/custom/purchasing/src/Plugin/Block/DiscountMatrixBlock.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Drupal\purchasing\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\node\NodeInterface;
+use Drupal\node\Entity\Node;
+use Drupal\purchasing\Model\DiscountMatrix;
+
+/**
+ * @Block(
+ *   id = "discount_matrix_block",
+ *   admin_label = @Translation("Discount Matrix Block")
+ * )
+ */
+class DiscountMatrixBlock extends BlockBase {
+
+  /**
+   * {@inheritDoc}
+   * @see \Drupal\Core\Block\BlockPluginInterface::build()
+   */
+  public function build() {
+    $node = \Drupal::routeMatch()->getParameter('node');
+    $procurement_bundles = [
+      'solicitation', 'contract', 'quote',
+    ];
+
+    if ($node instanceof NodeInterface && in_array($node->bundle(), $procurement_bundles)) {
+      $award_ids = \Drupal::entityQuery('node')
+        ->condition('type', 'award')
+        ->condition('field_procurement_method', $node->id())
+        ->condition('status', 1)
+        ->execute();
+
+      $line_item_ids = \Drupal::entityQuery('node')
+        ->condition('type', 'discounted_line_item')
+        ->condition('status', 1)
+        ->condition('field_award.target_id', $award_ids, 'IN')
+        ->execute();
+
+      if (!empty($line_item_ids)) {
+        $line_items = Node::loadMultiple($line_item_ids);
+
+        $matrix = new DiscountMatrix($line_items);
+
+        return [
+          '#theme' => 'discount_matrix',
+          '#matrix' => $matrix,
+        ];
+      }
+    }
+  }
+}
