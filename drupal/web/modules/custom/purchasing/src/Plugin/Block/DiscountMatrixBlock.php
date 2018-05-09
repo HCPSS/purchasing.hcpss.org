c45fd2282a36b2598924ca2df253fdfde9b51ec0
Brendan Anderson <brendan_anderson@hcpss.org>
2018-05-09 10:23:27 -0400

    Cache matrix block

--- drupal/web/modules/custom/purchasing/src/Plugin/Block/DiscountMatrixBlock.php
+++ drupal/web/modules/custom/purchasing/src/Plugin/Block/DiscountMatrixBlock.php
@@ -6,6 +6,7 @@ use Drupal\Core\Block\BlockBase;
 use Drupal\node\NodeInterface;
 use Drupal\node\Entity\Node;
 use Drupal\purchasing\Model\DiscountMatrix;
+use Drupal\Core\Cache\Cache;
 
 /**
  * @Block(
@@ -50,5 +51,31 @@ class DiscountMatrixBlock extends BlockBase {
         ];
       }
     }
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see \Drupal\Core\Plugin\ContextAwarePluginBase::getCacheTags()
+   */
+  public function getCacheTags() {
+    //With this when your node change your block will rebuild
+    if ($node = \Drupal::routeMatch()->getParameter('node')) {
+      //if there is node add its cachetag
+      return Cache::mergeTags(parent::getCacheTags(), array('node:' . $node->id()));
+    } else {
+      //Return default tags instead.
+      return parent::getCacheTags();
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   * @see \Drupal\Core\Plugin\ContextAwarePluginBase::getCacheContexts()
+   */
+  public function getCacheContexts() {
+    //if you depends on \Drupal::routeMatch()
+    //you must set context of this block with 'route' context tag.
+    //Every new route this block will rebuild
+    return Cache::mergeContexts(parent::getCacheContexts(), array('route'));
   }
 }
