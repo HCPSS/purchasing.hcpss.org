9676aadc817306c3e7549c417cf8b8ef8d5d4e9e
Brendan Anderson <brendan_anderson@hcpss.org>
2018-07-27 12:13:19 -0400

    Wait for MySQL

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -10,6 +10,12 @@ chown root:root /var/www/drupal/web/sites/default/settings.php
 chmod 444 /var/www/drupal/web/sites/default/services.yml
 chmod 444 /var/www/drupal/web/sites/default/settings.php
 
+# Wait for MySQL
+while ! mysqladmin ping -h"$MYSQL_HOSTNAME" --silent; do
+    echo "Waiting for database connection..."
+    sleep 5
+done
+
 drush --root=/var/www/drupal/web cr
 drush --root=/var/www/drupal/web updatedb -y
 drush --root=/var/www/drupal/web cim -y
