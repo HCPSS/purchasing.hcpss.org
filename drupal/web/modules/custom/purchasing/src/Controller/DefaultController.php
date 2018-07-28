a339bb3da9033916b73f07bbf04a7a94ebcf88ab
Brendan Anderson <brendan_anderson@hcpss.org>
2018-07-27 20:00:19 -0400

    Remove page manager

--- drupal/web/modules/custom/purchasing/src/Controller/DefaultController.php
+++ drupal/web/modules/custom/purchasing/src/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Drupal\purchasing\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\block_content\Entity\BlockContent;
 
 class DefaultController extends ControllerBase {
 
@@ -11,5 +12,15 @@ class DefaultController extends ControllerBase {
    */
   public function login() {
     return $this->redirect('simplesamlphp_auth.saml_login');
+  }
+
+  public function businessOpportunities() {
+    $output = [];
+
+    $block = BlockContent::load('block_content:a42d9b3d-68c7-452c-b769-6fcd3c41321b');
+    $output[] = \Drupal::entityTypeManager()->
+      getViewBuilder('block_content')->view($block);
+
+    return $output;
   }
 }
