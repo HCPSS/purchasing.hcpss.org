8ab92a5116fa34006da99376ce6787ef661d3de2
Brendan Anderson <brendan_anderson@hcpss.org>
2018-09-21 10:01:26 -0400

    Make address optional

--- drupal/web/modules/custom/purchasing/src/Validation/FundRaisingOrganizationValidator.php
+++ drupal/web/modules/custom/purchasing/src/Validation/FundRaisingOrganizationValidator.php
@@ -29,22 +29,5 @@ class FundRaisingOrganizationValidator extends EntityValidator {
         $this->addViolation(new Violation("Date ($date) is invalid."));
       }
     }
-
-    $address = $this->entity->field_address->first();
-    if (!$address->getAddressLine1()) {
-      $this->addViolation(new Violation('Address is required.'));
-    }
-
-    if (!$address->getLocality()) {
-      $this->addViolation(new Violation('City is required.'));
-    }
-
-    if (!$address->getAdministrativeArea()) {
-      $this->addViolation(new Violation('State is required.'));
-    }
-
-    if (!$address->getPostalCode()) {
-      $this->addViolation(new Violation('Zip is required.'));
-    }
   }
 }
