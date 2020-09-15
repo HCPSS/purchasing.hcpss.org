90bead535abed888b6ab484c53ffb5ca9b7fc993
Brendan Anderson <brendan_anderson@hcpss.org>
2020-09-15 15:23:37 -0400

    uninstall node_title_validation

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -16,7 +16,6 @@ while ! mysqladmin ping -h"$MYSQL_HOSTNAME" --silent; do
     sleep 5
 done
 
-drush --root=/var/www/drupal/web pmu file_entity -y
 drush --root=/var/www/drupal/web cc drush
 drush --root=/var/www/drupal/web cr
 drush --root=/var/www/drupal/web csim -y
